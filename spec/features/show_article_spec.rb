8a1fd69b624d48ee8b294ab5d94538b8f1d0fcca
hrm3vi <hrm3vi@gmail.com>
2018-08-19 00:11:33 -0500

    hide edit and delete article links for non-signed-in user or non-owners

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -3,18 +3,34 @@ require "rails_helper"
 RSpec.feature "Showing an article" do 
 
 	before do
-		john = User.create(email: 'john@example.com', password: 'pasword')
-		login_as(john)
-		@article = Article.create(title: 'The first article', body: 'Lorem ipsum lalala', user: john)
+		@john = User.create(email: 'john@example.com', password: 'pasword')
+		@chela = User.create(email: 'chela@example.com', password: 'password')
+		@article = Article.create(title: 'The first article', body: 'Lorem ipsum lalala', user: @john)
 	end
 
-	scenario "A user shows an article" do
+	scenario "to non-signed-in users, hide edit and delete button" do
 		visit "/"
 		click_link @article.title
-
 		expect(page).to have_content(@article.title)
 		expect(page).to have_content(@article.body)
 		expect(current_path).to eq(article_path(@article))
+
+		expect(page).not_to have_css("#edit-article")
+		expect(page).not_to have_css("#delete-article")
 	end
+
+	scenario "to non-owner, hide edit and delete button" do
+		login_as(@chela)
+		visit "/"
+		click_link @article.title
+		expect(page).to have_content(@article.title)
+		expect(page).to have_content(@article.body)
+		expect(current_path).to eq(article_path(@article))
+
+		expect(page).not_to have_css("#edit-article")
+		expect(page).not_to have_css("#delete-article")
+	end
+
+
 
 end
